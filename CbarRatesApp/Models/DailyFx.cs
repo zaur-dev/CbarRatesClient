87ea1532146b19edb28670d12c0ab83092cc4340
zaur-dev <zaur@zaur.dev>
2023-03-19 01:28:27 +0400

    Components added

---
+++ CbarRatesApp/Models/DailyFx.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FxCore.Models
+{
+    public class DailyFx
+    {
+        
+        public string Id { get; set; }
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime CbarDate { get; set; }
+        public List<Rate> Rates { get; set; }
+    }
+
+    public class Rate
+    {
+        public string Code { get; set; }
+        public int Nominal { get; set; }
+        public decimal Value { get; set; }
+    }
+}
