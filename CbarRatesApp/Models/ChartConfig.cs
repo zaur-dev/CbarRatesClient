09b8061334293155f0941d94979d80a650d2d348
zaur-dev <zaur@zaur.dev>
2023-03-24 18:37:17 +0400

    Used arrays instead of Lists

--- CbarRatesApp/Models/ChartConfig.cs
+++ CbarRatesApp/Models/ChartConfig.cs
@@ -4,25 +4,38 @@
     {
         public string Type { get; set; }
         public Data Data { get; set; }
+        //public Options Options { get; set; }
     }
 
     public class Data
     {
-        public string[] Labels { get; set; }
+        //public string[] Labels { get; set; }
         public Dataset[] Datasets { get; set; }
     }
 
     public class Dataset
     {
         public string Label { get; set; }
-        public string[] Data { get; set; }
-    } 
+        public DataItem[] Data { get; set; }
+    }
 
     public class DataItem
     {
         public string x { get; set; }
         public string y { get; set; }
     }
+
+    //public class Options
+    //{
+    //    public Parsing Parsing { get; set; }
+    //}
+
+    //public class Parsing
+    //{
+    //    public string xAxisKey { get; set; }
+    //    public string yAxisKey { get; set; }
+    //}
+
 
     public enum ChartType
     {
